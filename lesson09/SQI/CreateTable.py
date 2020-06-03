3378ef04b7ca1e7ba95978a1b34e1db336f22434
LE-S34 <r208790@yahoo.com.tw>
2020-06-03 19:20:05 +0800

    python

--- lesson09/SQI/CreateTable.py
+++ lesson09/SQI/CreateTable.py
@@ -1,6 +1,15 @@
 import sqlite3
 
-sql = ''
+sql = 'create table if not exists Lotto (' \
+        'id INTEGER PRIMARY KEY AUTOINCREMENT, ' \
+        'n1 INTEGER, ' \
+        'n2 INTEGER, ' \
+        'n3 INTEGER, ' \
+        'n4 INTEGER, ' \
+        'n5 INTEGER, ' \
+        'n6 INTEGER, ' \
+        'ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP)'
+
 
 conn = sqlite3.connect('demo.db')
 cursor = conn.cursor()
